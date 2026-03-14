1e634e2fbb328e2c13fc304480357625bb1e6b93
attar-code <attar.studycode@gmail.com>
2026-03-14 20:26:48 +0800

    pt 3

---
+++ POST-TEST-APL-3/2509106046-AhmadAttarAr-ryad-PT-3.cpp
@@ -0,0 +1,423 @@
+#include<iostream>
+using namespace std;
+
+struct serviceLaptop{
+    string MerekLaptop, KodeLaptop, keluhan, status;
+    int biaya;
+};
+
+struct RegisterUser{
+    string nama, nohp, password;
+    serviceLaptop service[100];
+    int JumlahService = 0;
+};
+
+RegisterUser user[100];
+int JumlahUser = 0;
+int role, USER, ADMIN, MenuUser, YangLogin, menuAdmin;
+string username, password;
+
+bool LoginAdmin(string namaadmin, string passwordadmin){
+    int login = 0;
+
+    cout << "LOGIN ADMIN (MAKSIMAL 3 KALI)" << endl;
+
+    while(login < 3){
+        cout << "Username : ";
+        cin >> username;
+
+        cout << "Password : ";
+        cin >> password;
+
+        if(username == namaadmin && password == passwordadmin){
+            cout << "BERHASIL MASUK" << endl;
+            return true;
+        }
+
+        cout << "USERNAME ATAU PASSWORD SALAH" << endl;
+        login++;
+    }
+
+    return false;
+}
+
+void DaftarUser(){
+    cout << endl << "SILAHKAN DAFTAR" << endl;
+
+    cin.ignore();
+    cout << "Nama : ";
+    getline(cin, user[JumlahUser].nama);
+
+    cout << "NoHp : ";
+    cin >> user[JumlahUser].nohp;
+
+    cout << "Password : ";
+    cin >> user[JumlahUser].password;
+
+    JumlahUser++;
+}
+
+void tambahBiaya(int userIndex, int serviceIndex){
+    cout << "Masukkan Biaya : ";
+    cin >> user[userIndex].service[serviceIndex].biaya;
+}
+
+void tambahBiaya(int userIndex, int serviceIndex, int tambahan){
+    int biaya;
+    cout << "Masukkan Biaya : ";
+    cin >> biaya;
+
+    user[userIndex].service[serviceIndex].biaya = biaya + tambahan;
+}
+
+void tampilServiceRekursif(int userIndex, int i){
+
+    if(i >= user[userIndex].JumlahService){
+        return;
+    }
+    cout << endl;
+    cout << "Service ke-" << i+1 << endl;
+    cout << "Merek Laptop : " << user[userIndex].service[i].MerekLaptop << endl;
+    cout << "Kode Laptop  : " << user[userIndex].service[i].KodeLaptop << endl;
+    cout << "Keluhan      : " << user[userIndex].service[i].keluhan << endl;
+    cout << "Status       : " << user[userIndex].service[i].status << endl;
+    cout << "Biaya        : Rp " << user[userIndex].service[i].biaya << endl;
+
+    tampilServiceRekursif(userIndex, i+1);
+}
+
+int main(){
+
+    do{
+        cout << "|================|" << endl;
+        cout << "| SERVICE LAPTOP |" << endl;
+        cout << "|================|" << endl;
+
+        cout << "1. USER" << endl;
+        cout << "2. ADMIN" << endl;
+        cout << "3. KELUAR" << endl;
+        cout << "PILIH (1/2/3) : ";
+        cin >> role;
+
+        switch(role){
+
+        case 1:{
+            do{
+                cout << endl;
+                cout << "|=====USER=====|" << endl;
+                cout << "|  1. DAFTAR   |" << endl;
+                cout << "|  2. LOGIN    |" << endl;
+                cout << "|  3. KELUAR   |" << endl;
+                cout << "|==============|" << endl;
+                cout << "PILIH (1/2/3) : ";
+                cin >> USER;
+
+                switch(USER){
+
+                case 1:
+                    DaftarUser();
+                    break;
+
+                case 2:{
+                    string namaLogin, passwordLogin;
+                    bool sesuai = false;
+                    int LOGIN = 0;
+
+                    while(LOGIN < 3){
+
+                        cin.ignore();
+                        cout << "Nama : ";
+                        getline(cin, namaLogin);
+
+                        cout << "Password : ";
+                        cin >> passwordLogin;
+
+                        for(int i=0;i<JumlahUser;i++){
+                            if(namaLogin == user[i].nama && passwordLogin == user[i].password){
+                                YangLogin = i;
+                                sesuai = true;
+                                break;
+                            }
+                        }
+
+                        if(sesuai){
+                            break;
+                        }
+                        else{
+                            LOGIN++;
+                            cout << "NAMA ATAU PASSWORD SALAH" << endl;
+                        }
+                    }
+
+                    if(sesuai){
+                        cout << "======LOGIN BERHASIL=======" << endl;
+
+                        do{
+                            cout << "|========MENU USER========|" << endl;
+                            cout << "|1. SERVICE LAPTOP        |" << endl;
+                            cout << "|2. RIWAYAT SERVICE LAPTOP|" << endl;
+                            cout << "|3. EDIT BIODATA          |" << endl;
+                            cout << "|4. BATALKAN SERVICE      |" << endl;
+                            cout << "|5. KELUAR                |" << endl;
+                            cout << "|=========================|" << endl;
+                            cout << "PILIH MENU (1/2/3/4/5) : ";
+                            cin >> MenuUser;
+
+                            switch(MenuUser){
+
+                            case 1:{
+                                cin.ignore();
+                                cout << "MEREK LAPTOP : ";
+                                getline(cin, user[YangLogin].service[user[YangLogin].JumlahService].MerekLaptop);
+
+                                cout << "KODE LAPTOP  : ";
+                                getline(cin, user[YangLogin].service[user[YangLogin].JumlahService].KodeLaptop);
+
+                                cout << "KELUHAN      : ";
+                                getline(cin, user[YangLogin].service[user[YangLogin].JumlahService].keluhan);
+
+                                user[YangLogin].service[user[YangLogin].JumlahService].status = "MENUNGGU";
+                                user[YangLogin].service[user[YangLogin].JumlahService].biaya = 0;
+
+                                user[YangLogin].JumlahService++;
+
+                                cout << "SERVICE BERHASIL DITAMBAHKAN\n";
+                                break;
+                            }
+
+                            case 2:{
+                                cout << "====BIODATA USER====" << endl;
+                                cout << "Nama : " << user[YangLogin].nama << endl;
+                                cout << "NoHp : " << user[YangLogin].nohp << endl;
+
+                                if(user[YangLogin].JumlahService == 0){
+                                    cout << "BELUM ADA SERVICE" << endl;
+                                }else{
+                                    tampilServiceRekursif(YangLogin,0);
+                                }
+                                break;
+                            }
+
+                            case 3:{
+                                int editBiodata;
+                                cout << "|====EDIT BIODATA====|" << endl;
+                                cout << "| 1. Ganti Nama      |" << endl;
+                                cout << "| 2. Ganti NoHp      |" << endl;
+                                cout << "| 3. Ganti Password  |" << endl;
+                                cout << "|====================|" << endl;
+                                cout << "Pilih : ";
+                                cin >> editBiodata;
+
+                                cin.ignore();
+                                if(editBiodata == 1){
+                                    cout << "Nama Baru : ";
+                                    getline(cin, user[YangLogin].nama);
+                                }
+                                else if(editBiodata == 2){
+                                    cout << "NoHp Baru : ";
+                                    cin >> user[YangLogin].nohp;
+                                }
+                                else if(editBiodata == 3){
+                                    cout << "Password Baru : ";
+                                    cin >> user[YangLogin].password;
+                                }
+                                else{
+                                    cout << "Menu tidak ada" << endl;
+                                }
+                                break;
+
+                            }
+
+                            case 4:{
+                                int hapus;
+
+                                if(user[YangLogin].JumlahService == 0){
+                                    cout << "BELUM ADA SERVICE" <<  endl;
+                                }else{
+                                    cout << "=====DATA SERVICE=====" << endl;
+
+                                    for(int i = 0; i < user[YangLogin].JumlahService;i++){
+                                        cout << i + 1 << ". " << user[YangLogin].service[i].MerekLaptop << " - " << user[YangLogin].service[i].keluhan << endl;
+                                    }
+
+                                    cout << "SERVICE MANA YANG INGIN DIHAPUS : ";
+                                    cin >> hapus;
+                                    hapus--;
+
+                                    if(hapus >=0 && hapus < user[YangLogin].JumlahService){
+
+                                        for(int i=hapus;i<user[YangLogin].JumlahService-1;i++){
+                                            user[YangLogin].service[i] =
+                                            user[YangLogin].service[i+1];
+                                        }
+
+                                        user[YangLogin].JumlahService--;
+                                        cout << "SERVICE BERHASIL DIBATALKAN" << endl;
+                                    }
+                                    else{
+                                        cout << "PILIHAN TIDAK VALID" << endl;
+                                    }
+                                }
+                                break;
+                            }
+
+                            }
+
+                        }while(MenuUser != 5);
+                    }
+                    else{
+                        cout << "GAGAL LOGIN 3 KALI" << endl;
+                    }
+
+                    break;
+                }
+
+                }
+
+            }while(USER != 3);
+            break;
+        }
+
+        case 2:{
+            if(LoginAdmin("admin","123")){
+
+                do{
+                    cout << "|=======MENU ADMIN======|" << endl;
+                    cout << "|1. Lihat Semua Service |" << endl;
+                    cout << "|2. Ubah Status Service |" << endl;
+                    cout << "|3. Tambah Biaya Service|" << endl;
+                    cout << "|4. Keluar              |" << endl;
+                    cout << "|=======================|" << endl;
+                    cout << "admin pilih yang mana : ";
+                    cin >> menuAdmin;
+
+                    switch(menuAdmin){
+                        case 1:{
+                            cout << endl << "====DATA SERVICE USER====" << endl;
+
+                            if(JumlahUser == 0){
+                                cout << "BELUM ADA USER YANG TERDAFTAR" << endl;
+                                break;
+                            }
+                            bool adaservice = false;
+                            for(int i = 0; i < JumlahUser; i++){
+                                if(user[i].JumlahService > 0){
+                                    adaservice = true;
+                                    break;
+                                }
+                            }
+
+                            if(!adaservice){
+                                cout << "BELUM ADA SERVICE YANG MASUK" <<  endl;
+                                break;
+                            }
+
+                            for(int i = 0; i < JumlahUser; i++){
+                                for(int j = 0; j < user[i].JumlahService; j++){
+                                    cout << endl;
+                                    cout << "User : " << user[i].nama << endl;
+                                    cout << "NoHp : " << user[i].nohp << endl;
+                                    cout << "Service ke-" << j+1 << endl;
+                                    cout << "Merek Laptop : " << user[i].service[j].MerekLaptop << endl;
+                                    cout << "Kode Laptop  : " << user[i].service[j].KodeLaptop << endl;
+                                    cout << "Keluhan      : " << user[i].service[j].keluhan << endl;
+                                    cout << "Status       : " << user[i].service[j].status << endl;
+                                    cout << "Biaya        : Rp " << user[i].service[j].biaya << endl;
+                                }
+                            }
+                            break; 
+                        }
+
+                        case 2:{
+                            if(JumlahUser == 0){
+                                cout << "BELUM ADA USER YANG TERDAFTAR" << endl;
+                                break;
+                            }
+                            bool adaservice = false;
+
+                            for(int i = 0; i < JumlahUser; i++){
+                                if(user[i].JumlahService > 0){
+                                    adaservice = true;
+                                    break;
+                                }
+                            }
+                            if(!adaservice){
+                                cout << "BELUM ADA SERVICE" <<  endl;
+                                break;
+                            } 
+                            cout << "=== DAFTAR USER ===" << endl;
+                            for(int i = 0; i < JumlahUser; i++){
+                                cout << i+1 << ". " << user[i].nama << endl;
+                            }
+                            int pilihUser, pilihService;
+                            cout << "Pilih User : ";
+                            cin >> pilihUser;
+                            cout << "Pilih Service ke- : ";
+                            cin >> pilihService;
+                            cin.ignore();
+                            cout << "Status Baru : ";
+                            getline(cin, user[pilihUser-1].service[pilihService-1].status);
+
+                            cout << "Status berhasil diubah" << endl;
+                            break;
+                        }
+
+                        case 3:{
+                            if(JumlahUser == 0){
+                                cout << "BELUM ADA USER YANG TERDAFTAR" << endl;
+                                break;
+                            }
+                            bool adaservice = false;
+
+                            for(int i = 0; i < JumlahUser; i++){
+                                if(user[i].JumlahService > 0){
+                                    adaservice = true;
+                                    break;
+                                }
+                            }
+                            if(!adaservice){
+                                cout << "BELUM ADA SERVICE" <<  endl;
+                                break;
+                            } 
+                            cout << "=== DAFTAR USER ===" << endl;
+                            for(int i = 0; i < JumlahUser; i++){
+                                cout << i+1 << ". " << user[i].nama << endl;
+                            }
+                            int pilihUser, pilihService;
+                            cout << "Pilih User : ";
+                            cin >> pilihUser;
+                            cout << "Pilih Service ke- : ";
+                            cin >> pilihService;
+                            tambahBiaya(pilihUser-1, pilihService-1, 10000);
+                            cout << "Biaya yang tercantum termasuk harga sparepart dan jasa sebesar Rp 10.000" << endl;
+                            cout << "Biaya berhasil ditambahkan" << endl;
+                            break;
+                        }
+
+                        case 4:{
+                            cout << "Admin keluar dulu." << endl;
+                            break;
+                        }default:{
+                            cout << "GAK ADA PILIHAN" << endl;
+                        }
+                    }
+                }while(menuAdmin != 4);
+
+            }
+            else{
+                cout << "GAGAL LOGIN ADMIN 3 KALI" << endl;
+            }
+            break;
+        }
+
+        case 3:{
+            cout << "KELUAR" << endl;
+            break;
+        }
+
+        }
+
+    }while(role != 3);
+
+    return 0;
+}
